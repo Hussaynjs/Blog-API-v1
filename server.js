e6076873b52999b715364946e3392b994eb64700
hussaini musa <hussainimusa566@gmail.com>
2022-08-27 21:27:12 -0700

    docs implementation

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ require('express-async-errors')
 
 const express = require('express')
 const app = express()
-const cookieParser = require('cookie-parser')
-const fileUpload = require('express-fileupload')
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // middleware
 const errorHandlerMiddleware = require('./src/middleware/errorHandler')
@@ -19,16 +28,29 @@ const userRouter = require('./src/routes/userRouter')
 const postRouter = require('./src/routes/postRouter')
 const commentRouter = require('./src/routes/commentRouter')
 
-const port = process.env.PORT || 5000
-app.get('/', (req, res) => {
-
-    res.send('blog api');
-})
-
-
+app.set('trust proxy', 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 60,
+    })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 app.use(cookieParser(process.env.JWT_SECRET))
 app.use(fileUpload())
 app.use(express.json())
+
+const port = process.env.PORT || 5000
+app.get('/', (req, res) => {
+    res.send('<h1>Blog   API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/posts', postRouter)
